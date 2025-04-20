6be312beb854bf82154735a4828ef16731e4a608
cshgoku1 <87035842+cshgoku1@users.noreply.github.com>
2025-04-20 23:03:43 +0100

    Update bankDataWebscraping.py

--- bankDataWebscraping.py
+++ bankDataWebscraping.py
@@ -48,7 +48,7 @@ class BankingWebdataScrapper:
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
     
         # Use undetected_chromedriver (uc) directly
-        return uc.Chrome(options=options)
+        return uc.Chrome(options=options,use_subprocess=False)
 
     
                 
