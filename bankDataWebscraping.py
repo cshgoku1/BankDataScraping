3469ff5d218e14ed7a1a83936726684b54b865bd
cshgoku1 <87035842+cshgoku1@users.noreply.github.com>
2025-04-20 23:56:16 +0100

    Update bankDataWebscraping.py

--- bankDataWebscraping.py
+++ bankDataWebscraping.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-import undetected_chromedriver as uc
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
